c04ba1bb4d84eefbb7157873e5f410d4dad47645
Luis Arano <me@mrluisarano.com>
2024-02-16 10:05:40 -0500

    Read me

--- Cam Dumper.py
+++ Cam Dumper.py
@@ -40,10 +40,9 @@ print(Fore.LIGHTGREEN_EX + """
 ║  ╠═╣║║║───║║║ ║║║║╠═╝║╣ ╠╦╝
 ╚═╝╩ ╩╩ ╩  ═╩╝╚═╝╩ ╩╩  ╚═╝╩╚═
 \033[1;37m+-----------------------------+
-| \033[1;31m[#] \033[1;37mDeveloper : Erfan Noori |
-| \033[1;31m[#] \033[1;37mInstagram : @n.erfvn    |
-| \033[1;31m[#] \033[1;37mTelegram : @Radar_db    |
-| \033[1;31m[#] \033[1;37mVersion : 1.0.1         |
+| \033[1;31m[#] \033[1;37mDeveloper : Luis Arano  |
+| \033[1;31m[#] \033[1;37mOriginal : https://github.com/erfannoori/Cam-Dumper    |
+| \033[1;31m[#] \033[1;37mVersion : 1.0.2         |
 +-----------------------------+
 """)
 
